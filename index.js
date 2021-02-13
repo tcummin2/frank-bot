cee1766160841181ff13b3a53d0886c13ec1b7bb
Tyler Cummings <tylercummings@comcast.net>
2021-02-13 13:42:56 -0500

    Play sound at random intervals

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const { token } = require('./config.json')
 
 const client = new Discord.Client()
+let timeout
 
 client.login(token)
   .then(() => console.log('Started'))
@@ -27,13 +28,34 @@ client.on('voiceStateUpdate', async (oldGuildMember, newGuildMember) => {
   const isBotOnlyOneInChannel = !!largestChannel && !largestChannel.members.filter(member => !member.user.bot).size
   if (isBotOnlyOneInChannel && guild.voiceConnection) {
     guild.voiceConnection.disconnect()
+    clearTimeout(timeout)
+    timeout = null
   } else {
     await largestChannel.join()
+    if (!timeout) queueSoundPlayback()
   }
 })
 
 client.on('message', async ({ content, channel }) => {
   if (content.toLowerCase() === 'sup frank') {
     await channel.send('sup dude')
+  } else if (content.toLowerCase() === 'bye frank') {
+    await channel.send('see ya')
   }
 })
+
+function playSound() {
+  client.voiceConnections.forEach(connection => {
+    const dispatcher = connection.playFile('assets/stop-it.mp3')
+    dispatcher.on('end', queueSoundPlayback)
+  })
+}
+
+function getRandomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function queueSoundPlayback() {
+  const ms = getRandomNumberBetween(30, 90) * 60 * 1000
+  timeout = setTimeout(playSound, ms)
+}
