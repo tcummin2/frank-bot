50bbc8988e239e482d4bec395289d70b84ed2dc7
Tyler Cummings <tylercummings@comcast.net>
2023-03-23 19:28:55 -0400

    Update discord.js to v14

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-const Discord = require('discord.js')
+const { Client, GatewayIntentBits, ChannelType } = require('discord.js')
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  getVoiceConnection,
+  AudioPlayerStatus
+} = require('@discordjs/voice')
 const { token, botId } = require('./config.json')
 
-const client = new Discord.Client()
+const SOUND_FILE = 'assets/stop-it.mp3'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
 let timeout
 
 client.login(token)
   .then(() => console.log('Started'))
 
 client.on('error', console.error)
+
+const player = createAudioPlayer()
+
+player.on('stateChange', (oldState, newState) => {
+  if (oldState.status === AudioPlayerStatus.Playing && newState.status === AudioPlayerStatus.Idle) {
+    queueSoundPlayback()
+  }
+})
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
   const oldUser = oldState.member.user
@@ -18,7 +45,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
 
   if (newUser.bot || oldUser.bot || newVoiceChannel === oldVoiceChannel) return
 
-  const voiceChannels = guild.channels.cache.filter(channel => channel.type === 'voice')
+  const voiceChannels = guild.channels.cache.filter(channel => channel.type === ChannelType.GuildVoice)
 
   const largestChannel = voiceChannels.reduce((currentMax, channel) => {
     let members = channel.members.filter(member => !member.user.bot)
@@ -29,13 +56,21 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
   }, null)
 
   const isBotOnlyOneInChannel = !!largestChannel && !largestChannel.members.filter(member => !member.user.bot).size
-  if (isBotOnlyOneInChannel && guild.voice.channel) {
-    guild.voice.channel.leave()
+  if (isBotOnlyOneInChannel) {
+    const connection = getVoiceConnection(guild.id)
+    connection?.destroy()
     clearTimeout(timeout)
     timeout = null
   } else {
     try {
-      await largestChannel.join()
+      const connection = joinVoiceChannel({
+        channelId: largestChannel.id,
+        guildId: largestChannel.guild.id,
+        adapterCreator: largestChannel.guild.voiceAdapterCreator,
+        selfDeaf: false,
+        setMute: false
+      })
+      connection.subscribe(player)
       if (!timeout) queueSoundPlayback()
     } catch (e) {
       // do nothing
@@ -43,13 +78,13 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
   }
 })
 
-client.on('message', async ({ content, channel, mentions }) => {
+client.on('messageCreate', async ({ content, channel, mentions }) => {
   if (content.toLowerCase() === 'sup frank') {
-    await channel.send('sup dude')
+    await channel.send({ content: 'sup dude' })
   } else if (content.toLowerCase() === 'bye frank') {
-    await channel.send('see ya')
+    await channel.send({ content: 'see ya' })
   } else if (mentions.members.some(member => member.id === botId)) {
-    await channel.send('', {
+    await channel.send({
       files: [{
         attachment: 'assets/tip.jpg',
         name: 'tip.jpg'
@@ -59,10 +94,8 @@ client.on('message', async ({ content, channel, mentions }) => {
 })
 
 function playSound() {
-  client.voice.connections.forEach(connection => {
-    const dispatcher = connection.play('assets/stop-it.mp3')
-    dispatcher.on('finish', queueSoundPlayback)
-  })
+  const resource = createAudioResource(SOUND_FILE)
+  player.play(resource)
 }
 
 function getRandomNumberBetween(min, max) {
